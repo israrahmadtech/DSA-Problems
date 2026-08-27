2153b7f3a5283d326c05063dc4eb376c2c4c81d5
israrahmadtech <israrahmadtech@gmail.com>
2026-08-27 17:46:54 +0500

    Two Sum III Enhance

--- TwoSum/two-sum-III.js
+++ TwoSum/two-sum-III.js
@@ -5,7 +5,7 @@ function twoSumThree(numbers) {
     let j;
     let k;
     for (let i = 0; i < numbers.length -2; i++) {
-        if(numbers[i] > 0) break
+        if(numbers[i] > 0 || numbers[numbers.length-1] < 0) break
         if (i > 0 && numbers[i] == numbers[i - 1]) continue
         j = i + 1
         k = numbers.length - 1
@@ -33,3 +33,37 @@ function twoSumThree(numbers) {
 console.log(twoSumThree([-1, 0, 1, 2, -1, -4]));
 console.log(twoSumThree([0, 1, 1]));
 console.log(twoSumThree([0, 0, 0]));
+
+
+function practiceTwoSumIII(numbers){
+    numbers.sort((a, b) => a-b)
+    let sum
+    const triplets = []
+    let j;
+    let k;
+    for(let i = 0; i < numbers.length -2; i++){
+        if(numbers[i] > 0 || numbers[numbers.length-1] < 0) break
+        if(i > 0 && numbers[i] === numbers[i-1]) continue;
+        j = i+1;
+        k = numbers.length -1;
+        while(j < k){
+            sum = numbers[i] + numbers[j] + numbers[k]
+            if(sum === 0 && sum !== undefined){
+                triplets.push([numbers[i], numbers[j], numbers[k]])
+                j++; k--;
+                while(j < k && numbers[j] === numbers[j-1]){
+                    j++;
+                }
+                while(j < k && numbers[k] === numbers[k+1]){
+                    k--;
+                }
+            }
+            else if(sum > 0) k--;
+            else j++
+        }
+    }
+    return triplets
+}
+console.log(practiceTwoSumIII([-1, 0, 1, 2, -1, -4]));
+console.log(practiceTwoSumIII([0, 1, 1]));
+console.log(practiceTwoSumIII([0, 0, 0]));
